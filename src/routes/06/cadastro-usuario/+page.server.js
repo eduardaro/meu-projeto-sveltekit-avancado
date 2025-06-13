d562d4cf278d2cceb500dfb325afe814e7c046be
eduardaro <eduarda.rocha@estudante.ifms.edu.br>
2025-06-13 08:30:37 -0400

    usuario

--- src/routes/06/cadastro-usuario/+page.server.js
+++ src/routes/06/cadastro-usuario/+page.server.js
@@ -1,22 +1,49 @@
 import { fail, redirect } from '@sveltejs/kit';
 
+/** retorna true se a string contem pelo menos um dos caractereres */
+function contem(string, caracteres) {
+  for (const caractere of caracteres) {
+    if (string.includes(caractere)) return true;
+  }
+  return false;
+
+}
+
 export const actions = {
   default: async ({ request }) => {
     const data = await request.formData();
-    const nome = data.get('nome');
-    const email = data.get('email');
-    const nascimento = data.get('nascimento');
+    const dados = {
+    nome : data.get('nome'),
+     email : data.get('email'),
+    nascimento : data.get('nascimento'),
+    senha : data.get('senha'),
+     confirmacaosenha : data.get('confirmacaosenha'),
+    erros: []
+    }
+
+    if (!dados.nome || !dados.email || !dados.nascimento || !dados.senha || !dados.confirmacaosenha)
+      dados.erros.push('Preencha todos os campos.');
+
+    if (!dados.email.includes('@'))
+      dados.erros.push ('Email inválido.');
+
+    if (!dados.senha != dados.confirmacaosenha) dados.erros.push('Senha e confirmação de senha não conferem.');
+      return fail(400, { error: "Senha não conferem." })
+
+    if (!contem(senha, "abcdefghijklmnopqrstuvwxyz")
+      || !contem(senha, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+      || !contem(senha, "0123456789")
+      || !contem(senha, "!@#$%¨&*()-_=+")
+
+    )
+
+      return fail(400, { error: 'A senha deve ter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial' })
 
 
-    if (!nome || !email || !nascimento ) return fail(400, { error: 'Preencha todas os campos.', nome,  email, nascimento });
-
-    if (!email.includes('@')) return fail(400, { error: 'Email inválido.', email });
-
-
-    let agora= new Date();
+    let agora = new Date();
     let nasc = new Date(nascimento)
     if (agora - nasc < 378691200000)
-        return fail (400, {error: 'Você ainda não completou 12 anos!', nome, email, nascimento});
+      return fail(400, { error: 'Você ainda não completou 12 anos!', nome, email, nascimento });
 
     redirect(303, '/06/profile');
   }
--- src/routes/06/cadastro-usuario/+page.svelte
+++ src/routes/06/cadastro-usuario/+page.svelte
@@ -1,16 +1,26 @@
 <script>
-    let { form } = $props();
-  </script>
-  
-  <h2>Cadastro do Úsuario</h2>
-  <form method="POST">
-    <input name="nome" type="text" placeholder="Nome de usuário" value={form?.email || ''} required />
-    <input name="email" type="email" placeholder="E-mail" required />
-    <input name="nascimento" type="date" placeholder="Data de nascimento" required />
+  let { form } = $props();
+</script>
 
-    <button>Entrar</button>
-  </form>
-  
-  {#if form?.error}
-    <p style="color: red">{form.error}</p>
-  {/if}
+<h2>Cadastro do Úsuario</h2>
+<form method="POST">
+  <input name="nome" type="text" placeholder="Nome de usuário" value={form?.nome || ''} required />
+  <input name="email" type="email" placeholder="E-mail de usuário" value={form?.email || ''} required />
+  <input name="senha" type="password" placeholder="Senha" value={form?.senha || ''} required />
+  <input
+    name="confirmacaosenha"
+    type="password"
+    placeholder="Confirmação de senha"
+    value={form?.confimacaosenha || ''}
+    required
+  />
+  <input name="nascimento" type="date" value={form?.nascimento || ''} required />
+
+  <button>Cadastrar</button>
+</form>
+
+{#if form?.erros.length > 0}
+  {#each form.erros as erro}
+    <p style="color: red">{erro}</p>
+  {/each}
+{/if}
